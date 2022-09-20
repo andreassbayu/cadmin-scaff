59cc6b836837efa597aaa4125f0848c7fbd141fb
albertus <albertus@webarq.co.id>
2022-09-20 13:51:31 +0700

    add service provider

---
+++ src/CadminScaff/CadminScaffServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CadminScaff;
+
+use Illuminate\Support\ServiceProvider;
+
+class CadminScaffServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
