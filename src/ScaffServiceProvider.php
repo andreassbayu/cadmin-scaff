cf459fb70e764464f1a5f7e851d475419c14e631
albertus <albertus@webarq.co.id>
2022-09-21 09:24:02 +0700

    service provider

---
+++ src/ScaffServiceProvider.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace AndreassBayu\CadminScaff;
+
+use Illuminate\Support\Facades\Http;
+
+class ScaffServiceProvider {
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+    
+}
